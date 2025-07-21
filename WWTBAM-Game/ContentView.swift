d08f88fdffc34524c93ed239bac278b824c0aece
Arthur <Fokusnik90999@gmail.com>
2025-07-21 17:35:29 +0500

    Added Font

--- WWTBAM-Game/ContentView.swift
+++ WWTBAM-Game/ContentView.swift
@@ -10,15 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("Hello, world!")
+                .font(.sfCompact(.regular, size: 24))
+                
         }
         .padding()
+        .onAppear() {
+            for family in UIFont.familyNames.sorted() {
+                let names = UIFont.fontNames(forFamilyName: family)
+                print("Family: \(family), Names: \(names)") }
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+// Family: SF Compact, Names: ["SFCompact-Regular", "SFCompact-Ultralight", "SFCompact-Thin", "SFCompact-Light", "SFCompact-Medium", "SFCompact-Semibold", "SFCompact-Bold", "SFCompact-Heavy", "SFCompact-Black"]
+
---
+++ WWTBAM-Game/Helpers/FontExtension.swift
@@ -0,0 +1,23 @@
+//
+//  FontExtension.swift
+//  WWTBAM-Game
+//
+//  Created by Артур  Арсланов on 21.07.2025.
+//
+
+import SwiftUI
+
+extension Font {
+    enum SFCompact: String {
+        case regular = "SFCompact-Regular"
+        case semibold = "SFCompact-Semibold"
+    }
+
+    static func sfCompact(_ style: SFCompact, size: CGFloat = 24) -> Font {
+        return .custom(style.rawValue, size: size)
+    }
+}
+
+
+// Example
+// .font(.sfCompact(.regular, size: 24))
