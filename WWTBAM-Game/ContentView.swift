bd48039ea443910d0d2e42bb8d14bf142ec0167b
Arthur <Fokusnik90999@gmail.com>
2025-07-21 16:31:24 +0500

    Initial commit

---
+++ WWTBAM-Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WWTBAM-Game
+//
+//  Created by Артур  Арсланов on 21.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WWTBAM-Game/WWTBAM_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWTBAM_GameApp.swift
+//  WWTBAM-Game
+//
+//  Created by Артур  Арсланов on 21.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct WWTBAM_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
