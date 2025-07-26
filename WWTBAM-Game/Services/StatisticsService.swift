05b0db0eb2193b1e18af4a4b605a87e311c84fef
Arthur <Fokusnik90999@gmail.com>
2025-07-26 17:26:51 +0500

    add more rules

--- WWTBAM-Game/Services/StatisticsService.swift
+++ WWTBAM-Game/Services/StatisticsService.swift
@@ -4,4 +4,22 @@
 //
 //  Created by Артур  Арсланов on 21.07.2025.
 //
+import Foundation
 
+final class StatisticsService {
+    private enum Keys: String {
+        case BestGame
+    }
+    
+    var bestGame: Int {
+        get {
+            UserDefaults.standard.integer(forKey: Keys.BestGame.rawValue)
+        }
+        set {
+            let currentBest = UserDefaults.standard.integer(forKey: Keys.BestGame.rawValue)
+            if newValue > currentBest {
+                UserDefaults.standard.set(newValue, forKey: Keys.BestGame.rawValue)
+            }
+        }
+    }
+}
